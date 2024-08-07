ffaa39236a7cc791fd7df9e050284910c3d7f270
Wrong Answer <3046524346@qq.com>
2024-08-07 23:08:56 +0800

    style: 结构调整

--- Services/Api/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ Services/Api/Api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54dce9c031d3030c506de753fc377768d9795755")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23525de871b8d7eeb1af85885c15923951dbe9e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
